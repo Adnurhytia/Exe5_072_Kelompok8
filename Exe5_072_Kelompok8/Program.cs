2d9071ce4db5eb8e01f5c8a7c11537b4244d8829
Adnurhytia <adindanurhayati30@gmail.com>
2022-12-23 16:02:49 +0700

    Add class Node

--- Exe5_072_Kelompok8/Program.cs
+++ Exe5_072_Kelompok8/Program.cs
@@ -1,4 +1,18 @@
 ﻿using System;
 
 namespace Exe5_072_Kelompok3
-{ }
+{
+    class Node
+    {
+       
+        public string nama;
+        public Node next;
+        public Node(string i, Node  n)
+        {
+            nama = i;
+            next = n;
+        }
+    }
+
+
+}
